417587920068224b80426a583fe0a967aff28f9a
Roy Luo <r.luo2418@gmail.com>
2020-08-22 17:42:03 +1200

    fixed database issues

--- app/models.py
+++ app/models.py
@@ -6,23 +6,26 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True) 
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    calendar = db.relationship('SemesterCalendar', lazy='dynamic')
+    calendar = db.relationship('Calendar')
 
     def __repr__(self): 
         return 'User {}'.format(self.username)
 
-class SemesterCalendar(db.Model): 
+class Calendar(db.Model): 
     id = db.Column(db.Integer, primary_key=True) 
     start_date = db.Column(db.DateTime, default=datetime.utcnow())
     end_date = db.Column(db.DateTime, default=datetime.utcnow())
-    weeks = db.relationship('Week', lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    weeks = db.relationship('Week')
 
 class Week(db.Model): 
     id = db.Column(db.Integer, primary_key=True) 
     week_name = db.Column(db.String(64))
-    list_of_todos = db.relationship('ToDoItem', lazy='dynamic')
+    calendar_id = db.Column(db.Integer, db.ForeignKey('calendar.id'))
+    list_of_todos = db.relationship('ToDoItem')
 
 class ToDoItem(db.Model): 
     id = db.Column(db.Integer, primary_key=True) 
     description = db.Column(db.String(128))
+    week_id = db.Column(db.Integer, db.ForeignKey('week.id'))
 
