b7131c980fa7a8916e5548f6bc6fc5c64778d1b0
Roy Luo <r.luo2418@gmail.com>
2020-08-22 17:49:01 +1200

    adding database instance and models for flask shell sesssions

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import User, Calendar, Week, ToDoItem
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Calendar': Calendar, 'Week': Week, 'ToDoItem': ToDoItem}
