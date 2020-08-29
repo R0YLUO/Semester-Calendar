53221863ec2f1fa05b614ca15b2595c33ec191b5
Roy Luo <r.luo2418@gmail.com>
2020-08-29 18:22:32 +1200

    adding error checking when submitting semester dates

--- app/forms.py
+++ app/forms.py
@@ -36,11 +36,19 @@ class NewCalendarForm(FlaskForm):
     def validate_end_date(self, end_date): 
         if end_date.data.weekday() != 4:  
             raise ValidationError("Ending date for semester should be a Friday.")
+        if end_date.data <= self.start_date.data: 
+            raise ValidationError("Semester end date is earlier than semester start date")
     
     def validate_break_start(self, break_start): 
         if break_start.data.weekday() != 0: 
             raise ValidationError("Starting date for mid-semester break should be a Monday.")
+        if not (self.start_date.data < break_start.data < self.end_date.data): 
+            raise ValidationError("Semester break should be within the semester period.")
 
     def validate_break_end(self, break_end): 
         if break_end.data.weekday() != 4: 
             raise ValidationError("Ending date for mid-semester break should be a Friday.")
+        if break_end.data <= self.break_start.data: 
+            raise ValidationError("Break end date should be after break start date.")
+        if break_end.data >= self.end_date.data: 
+            raise ValidationError("Semester break should be within the semester period.")
