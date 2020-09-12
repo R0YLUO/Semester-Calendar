8752676560bbefa28d9eca3edbcd2b8392bb0fda
Roy Luo <r.luo2418@gmail.com>
2020-09-12 15:25:52 +1200

    changing db configuration for azure sql

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask
-from config import Config
+# from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import MetaData
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+import os 
+import urllib.parse
+
+db_string = os.environ['dbstring']
+params = urllib.parse.quote_plus(db_string)
 
 app = Flask(__name__)
-app.config.from_object(Config)
+# app.config.from_object(Config)
+
+app.config['SECRET_KEY'] = 'supersecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 
@@ -19,7 +29,7 @@ naming_convention = {
     "pk": "pk_%(table_name)s"
 }
 db = SQLAlchemy(app, metadata=MetaData(naming_convention=naming_convention))
-mirgrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
