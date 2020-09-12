281ad9d9225f68987ae2d5a1626faeb938f2b6f7
Roy Luo <r.luo2418@gmail.com>
2020-09-12 17:00:26 +1200

    setting up config for heroku deployment

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,25 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 app.config.from_object(Config)
 
+if not app.debug and not app.testing:
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log',
+                                            maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
 # app.config['SECRET_KEY'] = 'supersecret'
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
 # app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
